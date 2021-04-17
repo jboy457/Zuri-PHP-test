af03dd3377bee44b047ede20ce2c14f2709d72f8
jboy457 <adejareemma@gmail.com>
2021-04-17 16:57:54 +0100

    chore: handle resgistration

--- register.php
+++ register.php
@@ -1,22 +1,40 @@
 <?php
     session_start();
+    if (!empty($_SESSION)) {
+        header('location: home.php');
+    }
+    include 'function.php';
+
     $error = [];
-    
+
     if(array_key_exists('submit', $_POST)) {
 
         // Valdiate inputs
         if(empty($_POST['fname'])) {
             $error['fname'] = 'Firstname is required';
-        } elseif (empty($_POST['lname'])) {
+        } 
+        if (empty($_POST['lname'])) {
             $error['lname'] = 'Lastname is required';
-        } elseif (empty($_POST['email'])) {
+        } 
+        if (empty($_POST['email'])) {
             $error['email'] = 'Email is required';
-        } elseif (empty($_POST['phone'])) {
+        } 
+        if (empty($_POST['phone'])) {
             $error['phone'] = 'Phone is required';
-        } elseif (empty($_POST['pass'])) {
+        } 
+        if (empty($_POST['pass'])) {
             $error['pass'] = 'Password is required';
-        } else {
+        }
 
+        if(empty($error)) {
+            $registered = register($_POST);
+            if(!$registered) {
+                $error['register'] = "Users already registered";
+            } else {
+                $_SESSION = $_POST;
+                // var_dump($_SESSION['fname']);
+                header('location: home.php');
+            }
         }
     }
 
@@ -40,18 +58,28 @@
 </head>
 <body>
     <h1>Register here for Zuri </h1>
-    <form action="login.php" method="POST">
+    <?= field_error('register', $error) ?>
+    <form action="register.php" method="POST">
         <p>Firstname</p>
         <input type="text" name="fname" id="" placeholder="firstname">
+        <?= field_error('fname', $error) ?>
         <p>Lastname</p>
         <input type="text" name="lname" id="" placeholder="lastname">
+        <?= field_error('lname', $error) ?>
+
         <p>Email</p>
         <input type="text" name="email" id="" placeholder="Enter Email">
+        <?= field_error('email', $error) ?>
+
         <p>Phone</p>
         <input type="text" name="phone" id="" placeholder="Enter Phone">
+        <?= field_error('phone', $error) ?>
+
         <p>Password</p>
         <input type="password" name="pass" id="">
-        <input type="submit" name="submit" value="Register">
+        <?= field_error('pass', $error) ?>
+        <br>
+        <input type="submit" style="margin: 10px;" name="submit" value="Register">
     </form>
 </body>
 </html>
